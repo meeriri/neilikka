b0c657f0821835ed29dbb700a6b58107ba451b12
meeriri <meeri.rivakka@gmail.com>
2022-02-15 15:34:06 +0200

    polku toimivaksi muuallakin kuin localhostilla

--- yhteispalikat/polku.php
+++ yhteispalikat/polku.php
@@ -1,11 +1,18 @@
 <?php
-// tämä koodi kopioituna seuraaviin tiedostoihin:
-// header, latauslinkit
-    $projekti = "koulu/neilikka";
-    if (isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] === "on")
-        {$polku = "https://";}
-    else
-        {$polku = "http://";}
-    $polku .= $_SERVER["SERVER_NAME"] . "/" . $projekti;
-        // edellä SERVER_NAMEn tilalla voisi olla HTTP_HOST
+    // Projektikansion nimi:
+    $projekti = "neilikka";
+
+    // Jos sivua kutsuva (remote) palvelin on tarjoava (local) palvelin:
+    if (in_array($_SERVER["REMOTE_ADDR"], array("127.0.0.1","REMOTE_ADDR"=>"::1"))) {
+        $polku = "http://localhost/koulu";
+    }
+    else { // Jos kutsu tulee muualta, huomioidaan yhteyden tyyppi (https vai http):
+        $polku = (isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] === "on") ? "https://" : "http://";
+        $polku .= $_SERVER["SERVER_NAME"]; // ja lisätään palvelimen osoite
+            // (edellä SERVER_NAMEn tilalla voisi olla HTTP_HOST)
+    }
+
+    // Lisätään polun loppuun projektikansion nimi:
+    $polku .= "/$projekti"; 
+    echo $polku;
 ?>
